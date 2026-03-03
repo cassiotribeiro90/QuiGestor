3b4aa051dcb9b94bdb68395d718e81ecd9205eea
cassio <cassiotribeiro.90@gmail.com>
2026-03-03 11:28:45 -0300

    correcao card

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'injection.dart';
-import 'core/theme/app_theme.dart';
-import 'core/theme/theme_cubit.dart';
 import 'features/auth/bloc/auth_cubit.dart';
 import 'features/auth/views/splash_screen.dart';
 import 'features/auth/views/login_screen.dart';
@@ -15,23 +13,19 @@ class QuiGestorApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider<ThemeCubit>(create: (_) => getIt<ThemeCubit>()),
         BlocProvider<AuthCubit>(create: (_) => getIt<AuthCubit>()),
       ],
-      child: BlocBuilder<ThemeCubit, ThemeState>(
-        builder: (context, themeState) {
-          return MaterialApp(
-            title: 'QuiGestor',
-            debugShowCheckedModeBanner: false,
-            theme: AppTheme.lightTheme,
-            darkTheme: AppTheme.darkTheme,
-            themeMode: themeState.themeMode,
-            home: const SplashScreen(),
-            routes: {
-              '/login': (context) => const LoginScreen(),
-              '/home': (context) => const HomeScreen(),
-            },
-          );
+      child: MaterialApp(
+        title: 'QuiGestor',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+          useMaterial3: true,
+        ),
+        home: const SplashScreen(),
+        routes: {
+          '/login': (context) => const LoginScreen(),
+          '/home': (context) => const HomeScreen(),
         },
       ),
     );
