ecd8aa8b83560dea2983651bd5ad260fbef0d261
cassio <cassiotribeiro.90@gmail.com>
2026-03-10 17:28:27 -0300

    melhorias gestor

---
+++ lib/shared/mixins/navigation_mixin.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import '../../app/modules/home/views/home_screen.dart';
+
+mixin NavigationMixin<T extends StatefulWidget> on State<T> {
+  bool get isWeb => MediaQuery.of(context).size.width > 600;
+
+  void navigateToWebContent(Widget content, String title) {
+    if (isWeb) {
+      final homeState = context.findAncestorStateOfType<HomeScreenState>();
+      if (homeState != null) {
+        homeState.navigateTo(content, title);
+      }
+    }
+  }
+
+  Future<R?> navigateToMobileScreen<R>(Widget screen) {
+    return Navigator.push<R>(
+      context,
+      MaterialPageRoute(builder: (_) => screen),
+    );
+  }
+
+  void navigateToDetail({
+    required BuildContext context,
+    required Widget webContent,
+    required Widget mobileScreen,
+    required String title,
+  }) {
+    if (isWeb) {
+      final homeState = context.findAncestorStateOfType<HomeScreenState>();
+      if (homeState != null) {
+        homeState.navigateTo(webContent, title);
+      }
+    } else {
+      Navigator.push(context, MaterialPageRoute(builder: (_) => mobileScreen));
+    }
+  }
+}
